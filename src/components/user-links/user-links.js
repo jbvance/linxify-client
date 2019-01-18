48b1ca2f45e69ab8f7535db709474b4bbb3148f3
Jason Vance <=>
2019-01-18 07:40:06 -0600

    update user-links to show no links found message

--- src/components/user-links/user-links.js
+++ src/components/user-links/user-links.js
@@ -4,12 +4,16 @@ import LinkRow from '../link-row/link-row';
 import './user-links.css';
 
 const  UserLinks = props => { 
+        
         return (                  
-            <div>         
-                <section className="container links-container">            
-                    {props.links.map(link => <LinkRow key={link._id} link={link} deleteLink={props.deleteLink} />)}
-                </section>
-            </div>
+            <section className="container links-container"> 
+            {!props.links || props.links.length === 0 ? (
+                <div>No links found</div>
+              ) : (
+                props.links.map(link => <LinkRow key={link._id} link={link} deleteLink={props.deleteLink} />)
+              )}                         
+            </section>
+           
         );
 }
     
